0c45e35a30adee20f6beb4402aca81fca148d41b
DimaMalah <dmitryaronov77@gmail.com>
2022-04-23 00:08:19 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const sum = (a, b) => {
+	const summ = a + b;
+	return summ;
+}
+const sub = (a, b) => {
+	const subs = a - b;
+	return subs;
+}
+const mul = (a, b) => {
+	const mult = a * b;
+	return mult;
+}
